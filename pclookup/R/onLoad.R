bfa507474d59da30f20d00d19bc9eb90b0703d13
chrisbrunsdon <brunsdon@liverpool.ac.uk>
2013-04-11 14:49:40 +0100

    Revert "Package directories for postcoder and pclookup"
    This reverts commit ebdaa55f7f07b1b3a8950e9c4d8806eb470c7d9b.

--- pclookup/R/onLoad.R
+++
@@ -1,7 +0,0 @@
-.onLoad <- function(libname,pkgname) {
-  msg <- 'Loading "postcoder" package:
-Contains Ordnance Survey Data (c) Crown Copyright and database right 2012
-Contains National Statistics Data (c) Crown Copyright and database right 2012
-'
-  packageStartupMessage(msg)
-}
